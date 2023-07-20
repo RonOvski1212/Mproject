5b3d40c3372a8a161eb81fa640ac0854cd3ddf95
RonOvski1212 <245785gio@gmail.com>
2023-07-20 22:03:55 +0400

    add Timer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
+  // Tabs
+
   const tabs = document.querySelectorAll(".tabheader__item"),
     tabsContent = document.querySelectorAll(".tabcontent"),
     tabsParent = document.querySelector(".tabheader__items");
@@ -35,4 +37,47 @@ window.addEventListener("DOMContentLoaded", () => {
       });
     }
   });
+
+  //Timer : datainput იდან მიიღებს მილიწამებს, აკონვერტებს ტაიმერისთვის და აბრუნებს მასივად
+  const deadline = "2023-09-11";
+
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(t / (1000 * 60 * 60 * 24)),
+      hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+      minutes = Math.floor((t / (1000 * 60)) % 60),
+      seconds = Math.floor((t / 1000) % 60);
+
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+  // Timer მასივიდან იღებს და html -ზე გამოაქვს
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"),
+      timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      days.innerHTML = t.days;
+      hours.innerHTML = t.hours;
+      minutes.innerHTML = t.minutes;
+      seconds.innerHTML = t.seconds;
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock(".timer", deadline);
 });
