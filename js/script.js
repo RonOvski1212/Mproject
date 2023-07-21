6cde0bb5993ad10acd44d5002f958790d38852be
RonOvski1212 <245785gio@gmail.com>
2023-07-21 21:24:29 +0400

    functional modal window (not ALL)

--- js/script.js
+++ js/script.js
@@ -101,4 +101,21 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 
   setClock(".timer", deadline);
+
+  //modal
+  const modalTrigger = document.querySelector("[data-modal]"),
+    modal = document.querySelector(".modal"),
+    modalCloseBtn = document.querySelector("[data-close]");
+
+  modalTrigger.addEventListener("click", () => {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    document.body.style.overflow = "hidden"; // როცა გახსნილია მოდალური ფანჯარა არ აისქროლოს body
+  });
+
+  modalCloseBtn.addEventListener("click", () => {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    document.body.style.overflow = ""; // ბრაუზერი დეფაულტ-ს სვავს
+  });
 });
