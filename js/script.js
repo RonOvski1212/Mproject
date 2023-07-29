481be4756f1fef33cc1dae1334606c4a20d964c8
RonOvski1212 <245785gio@gmail.com>
2023-07-29 22:44:13 +0400

    modal message

--- js/script.js
+++ js/script.js
@@ -104,8 +104,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   //modal
   const modalTrigger = document.querySelectorAll("[data-modal]"),
-    modal = document.querySelector(".modal"),
-    modalCloseBtn = document.querySelector("[data-close]");
+    modal = document.querySelector(".modal");
 
   function openModal() {
     modal.classList.add("show");
@@ -124,10 +123,8 @@ window.addEventListener("DOMContentLoaded", () => {
     document.body.style.overflow = ""; // ბრაუზერი დეფაულტ-ს სვავს
   }
 
-  modalCloseBtn.addEventListener("click", closeModal);
-
   modal.addEventListener("click", (e) => {
-    if (e.target === modal) {
+    if (e.target === modal || e.target.getAttribute("data-close") == "") {
       closeModal();
     }
   });
@@ -139,7 +136,7 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  const modalTimerId = setTimeout(openModal, 6000);
+  const modalTimerId = setTimeout(openModal, 50000);
 
   function showModalByScroll() {
     // სქროლზე რეაგირებს და იძახებს მოდალურ ფანჯარას (- 1 პიქსელი დებაგია გარკვეული მონიტორებისთვის)
@@ -267,15 +264,37 @@ window.addEventListener("DOMContentLoaded", () => {
       request.addEventListener("load", () => {
         if (request.status === 200) {
           console.log(request.response);
-          statusMessage.textContent = message.success;
+          showThanksModal(message.success);
           form.reset();
-          setTimeout(() => {
-            statusMessage.remove();
-          }, 2000);
+          statusMessage.remove();
         } else {
-          statusMessage.textContent = message.failure;
+          showThanksModal(message.failure);
         }
       });
     });
+  }
+
+  function showThanksModal(message) {
+    const prevModalDialog = document.querySelector(".modal__dialog");
+
+    prevModalDialog.classList.add("hide");
+    openModal();
+
+    const thanksModal = document.createElement("div");
+    thanksModal.classList.add("modal__dialog");
+    thanksModal.innerHTML = `
+      <div class="modal__content">
+          <div class="modal__close" data-close>&times;</div>
+          <div class="modal__title">${message}</div>
+      </div>
+    `;
+
+    document.querySelector(".modal").append(thanksModal);
+    setTimeout(() => {
+      thanksModal.remove();
+      prevModalDialog.classList.add("show");
+      prevModalDialog.classList.remove("hide");
+      closeModal();
+    }, 4000);
   }
 });
